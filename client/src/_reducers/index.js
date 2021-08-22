da5cebaab42f968f35a41a0324b4d27f01a08375
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-23 01:02:47 +0900

    add redux, reducers

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+//combineReducers로 다양한 reducers를 하나로
+//합쳐준다
+
+// import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    //user,
+})
+
+export default rootReducer;
