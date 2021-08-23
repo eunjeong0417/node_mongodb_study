87cbd804114a2b69ae99ad3d30a30ed20ae4ed03
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-23 23:21:07 +0900

    auth페이지, authentication check 기능

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {LOGIN_USER,REGISTER_USER} from './types'
+import {LOGIN_USER,REGISTER_USER,AUTH_USER} from './types'
 
 export function loginUser(dataSubmit) {
 
@@ -19,12 +19,20 @@ export function registerUser(dataSubmit) {
 
     const request = axios.post('/api/users/register', dataSubmit)
         .then(res => res.data)
-    //서버에서 받아온 데이터를
-    //request에 저장한다
-    //reducer로 데이터를 보내준다
 
     return {
         type: REGISTER_USER,
+        payload: request
+    }
+}
+
+export function auth() {
+
+    const request = axios.get('/api/users/auth')
+        .then(res => res.data)
+
+    return {
+        type: AUTH_USER,
         payload: request
     }
 }
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,31 @@
 import React,{useEffect} from 'react';
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello').then(res => { console.log(res) })
     },[])
 
+    const clickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(res => {
+                if (res.data.success) {
+                props.history.push('/login')
+                } else {
+                    alert('로그아웃 실패')
+            }
+        })
+    }
+
     return (
         <div>
             <h2>시작페이지</h2>
+
+            <button onClick={clickHandler}>로그아웃</button>
         </div>
     )
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
